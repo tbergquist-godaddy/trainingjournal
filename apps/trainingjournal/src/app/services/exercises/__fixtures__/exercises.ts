b892f5e74840d331aec2bd488e6d0065aa7b8b97
Trond Bergquist <tbergquist@godaddy.com>
2024-10-11 18:43:37 -0400

    add more mock exercises

--- apps/trainingjournal/src/app/services/exercises/__fixtures__/exercises.ts
+++ apps/trainingjournal/src/app/services/exercises/__fixtures__/exercises.ts
@@ -23,6 +23,28 @@ const userExerciseMap: Record<string, Array<IExercise>> = {
       name: 'Plank',
     },
   ],
+  'fbcb9d14-3360-4b1e-8d20-944f7a71e51f': [
+    {
+      id: '1',
+      name: 'Bench Press',
+    },
+    {
+      id: '2',
+      name: 'Squat',
+    },
+    {
+      id: '3',
+      name: 'Deadlift',
+    },
+    {
+      id: '4',
+      name: 'Pull-Up',
+    },
+    {
+      id: '5',
+      name: 'Plank',
+    },
+  ],
 };
 
 export default userExerciseMap;

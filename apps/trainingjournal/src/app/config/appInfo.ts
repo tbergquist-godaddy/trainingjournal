a2b1f3edf944dec1226de5c521dd4bb0fef84870
Trond Bergquist <tbergquist@godaddy.com>
2024-10-26 09:46:40 -0400

    use vercel url

--- apps/trainingjournal/src/app/config/appInfo.ts
+++ apps/trainingjournal/src/app/config/appInfo.ts
@@ -1,7 +1,14 @@
-if (
-  process.env.NEXT_PUBLIC_API_DOMAIN === undefined ||
-  process.env.NEXT_PUBLIC_WEBSITE_DOMAIN === undefined
-) {
+const getVercelBranchUrl = () => {
+  const branchUrl = process.env.VERCEL_BRANCH_URL;
+  if (branchUrl === undefined) {
+    return undefined;
+  }
+  return `https://${branchUrl}`;
+};
+const apiDomain = getVercelBranchUrl() ?? process.env.NEXT_PUBLIC_API_DOMAIN;
+const websiteDomain =
+  getVercelBranchUrl() ?? process.env.NEXT_PUBLIC_WEBSITE_DOMAIN;
+if (apiDomain === undefined || websiteDomain === undefined) {
   throw new Error(
     'NEXT_PUBLIC_API_DOMAIN and NEXT_PUBLIC_WEBSITE_DOMAIN must be defined'
   );
@@ -9,8 +16,8 @@ if (
 export const appInfo = {
   // learn more about this on https://supertokens.com/docs/thirdpartyemailpassword/appinfo
   appName: 'trainingjournal',
-  apiDomain: process.env.NEXT_PUBLIC_API_DOMAIN,
-  websiteDomain: process.env.NEXT_PUBLIC_WEBSITE_DOMAIN,
+  apiDomain,
+  websiteDomain,
   apiBasePath: '/api/auth',
   websiteBasePath: '/login',
 };

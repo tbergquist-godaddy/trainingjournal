78b4d083928c3b9d8f67ed7353323a7a8d959dae
Trond Bergquist <tbergquist@godaddy.com>
2024-10-31 15:16:33 -0400

    move list link logic to list component

--- apps/trainingjournal/src/app/exercises/components/exercise-list.tsx
+++ apps/trainingjournal/src/app/exercises/components/exercise-list.tsx
@@ -1,9 +1,7 @@
 import Section from '../../components/layout/section';
 import List from '../../components/list/list';
 import Typography from '../../components/typography/typography';
-import Link from 'next/link';
 import { getExercises } from '../../services/exercises/exercise-service';
-import styles from './exercise-list.module.css';
 
 export default async function ExerciseList() {
   const exercises = await getExercises();
@@ -11,15 +9,12 @@ export default async function ExerciseList() {
     <Section>
       <List>
         {exercises?.map((exercise) => (
-          <List.Item className={styles.listItem} key={exercise.id}>
-            <Link
-              className={styles.exerciseLink}
-              href={`/exercises/edit/${exercise.id}`}
-            >
+          <List.Item key={exercise.id}>
+            <List.Link href={`/exercises/edit/${exercise.id}`}>
               <Typography as="span" key={exercise.id}>
                 {exercise.name}
               </Typography>
-            </Link>
+            </List.Link>
           </List.Item>
         ))}
       </List>

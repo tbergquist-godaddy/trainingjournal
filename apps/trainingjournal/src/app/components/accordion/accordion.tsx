6dfdb7d8e32897af505805fb5edcd69b2b527efe
Trond Bergquist <tbergquist@godaddy.com>
2024-11-24 14:39:08 -0500

    close other accordions when one opens

--- apps/trainingjournal/src/app/components/accordion/accordion.tsx
+++ apps/trainingjournal/src/app/components/accordion/accordion.tsx
@@ -1,4 +1,17 @@
-import { ReactNode } from 'react';
+'use client';
+
+import {
+  Children,
+  createContext,
+  isValidElement,
+  ReactNode,
+  use,
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react';
 import styles from './accordion.module.css';
 import Chevron from '../icons/chevron/chevron';
 
@@ -6,17 +19,81 @@ type Props = {
   children: ReactNode;
 };
 
+type AccordionContextType = {
+  register: (index: number, ref: HTMLElement) => void;
+  closeOthers: (index: number) => void;
+};
+
+const AccordionContext = createContext<AccordionContextType | undefined>(undefined);
+
+const AccordionProvider = ({ children }: Props) => {
+  const [refMap, setRefMap] = useState<Map<number, HTMLElement>>(new Map());
+
+  const register = useCallback((index: number, ref: HTMLElement) => {
+    setRefMap(map => new Map(map).set(index, ref));
+  }, []);
+
+  const closeOthers = useCallback(
+    (index: number) => {
+      refMap.forEach((ref, i) => {
+        if (i !== index) {
+          console.log({ i, index, ref });
+          ref.removeAttribute('open');
+        } else {
+          ref.setAttribute('open', '');
+        }
+      });
+    },
+    [refMap],
+  );
+  const state = useMemo(() => ({ register, closeOthers }), [register, closeOthers]);
+  return <AccordionContext.Provider value={state}>{children}</AccordionContext.Provider>;
+};
+
+const useAccordion = () => {
+  const context = use(AccordionContext);
+  if (context === undefined) {
+    throw new Error('useAccordion must be used within a AccordionProvider');
+  }
+  return context;
+};
+
 export default function Accordion({ children }: Props) {
-  return <div className={styles.accordion}>{children}</div>;
+  return (
+    <AccordionProvider>
+      <div className={styles.accordion}>
+        {Children.map(children, (child, i) =>
+          isValidElement(child) ? <child.type {...child.props} index={i} /> : null,
+        )}
+      </div>
+    </AccordionProvider>
+  );
 }
 
 type ItemProps = Props & {
   title: ReactNode;
+  index?: number;
 };
 
-export function AccordionItem({ title, children }: ItemProps) {
+export function AccordionItem({ title, children, index }: ItemProps) {
+  const { register, closeOthers } = useAccordion();
+  const ref = useRef(null);
+  useEffect(() => {
+    if (ref.current && index != undefined) {
+      register(index, ref.current);
+    }
+  }, [register, index]);
+
   return (
-    <details className={styles.accordionItem}>
+    <details
+      ref={ref}
+      onToggle={e => {
+        if ('newState' in e && e.newState === 'open' && index != null) {
+          closeOthers(index);
+        }
+      }}
+      className={styles.accordionItem}
+    >
       <summary>
         <span>{title}</span>
         <span className={styles.accordionItem_Expand}>

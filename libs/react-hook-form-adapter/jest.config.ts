b0563c55a1bcd57c4e76146acd5da925cb047ce7
Trond Bergquist <tbergquist@godaddy.com>
2024-12-22 12:32:42 -0500

    add react-hook-form adapter

---
+++ libs/react-hook-form-adapter/jest.config.ts
@@ -0,0 +1,11 @@
+export default {
+  displayName: 'react-hook-form-adapter',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/libs/react-hook-form-adapter',
+  passWithNoTests: true, // TODO: Add tests
+};

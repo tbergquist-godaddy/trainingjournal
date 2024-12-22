6b9e1e8d85bdf72cb6f5c2ae2357ba008b84a8e3
Trond Bergquist <tbergquist@godaddy.com>
2024-12-22 12:49:50 -0500

    movie-db: add dropdown to select a list

--- apps/movie-db/src/app/home/components/lists.tsx
+++ apps/movie-db/src/app/home/components/lists.tsx
@@ -2,8 +2,33 @@
 
 import useSwr from 'swr';
 import { fetchLists } from '@/services/account';
+import { Select } from '@tbergq/components';
+import { SyntheticEvent, useState, useTransition } from 'react';
+import { useRouter, usePathname, useSearchParams } from 'next/navigation';
 
 export default function Lists() {
   const listResponse = useSwr('lists', fetchLists, { suspense: true });
-  return <pre>{JSON.stringify(listResponse, null, 2)}</pre>;
+  const data = listResponse.data?.results ?? [];
+  const searchParams = useSearchParams();
+  const [selectedList, setSelectedList] = useState<string>(searchParams.get('listId') ?? '');
+  const router = useRouter();
+  const pathname = usePathname();
+  const [, startTransition] = useTransition();
+
+  const handleChange = (e: SyntheticEvent<HTMLSelectElement>) => {
+    const listId = e.currentTarget.value;
+    startTransition(() => {
+      setSelectedList(listId);
+      router.push(`${pathname}?listId=${listId}`);
+    });
+  };
+  return (
+    <Select
+      label="Select your list"
+      name="lists"
+      options={data.map(list => ({ value: list.id.toString(), text: list.name }))}
+      onChange={handleChange}
+      value={selectedList}
+    />
+  );
 }

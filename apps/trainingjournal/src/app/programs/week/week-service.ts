db2ee2e54ebf47d8a4aceb26c46b7479dfe1e51a
Trond Bergquist <tbergquist@godaddy.com>
2025-01-18 15:55:32 -0500

    add clone week function in week service

--- apps/trainingjournal/src/app/programs/week/week-service.ts
+++ apps/trainingjournal/src/app/programs/week/week-service.ts
@@ -16,6 +16,56 @@ export async function createWeek(input: CreateWeekInput) {
   throw new Error('Unauthorized');
 }
 
+export async function cloneWeek(weekId: string) {
+  const userId = (await getSSRUserId()) ?? '';
+  const where = {
+    id: weekId,
+    Program: {
+      userId,
+    },
+  };
+  const [week, weekCount] = await Promise.all([
+    prisma.week.findUnique({
+      where,
+      include: {
+        days: {
+          include: {
+            PlannedExercise: true,
+          },
+        },
+      },
+    }),
+    prisma.week.count({ where: { Program: { is: { weeks: { some: { id: weekId } } } } } }),
+  ]);
+  if (!week) {
+    throw new Error('Unauthorized');
+  }
+  return prisma.week.create({
+    data: {
+      name: `Week ${Number(weekCount) + 1}`,
+      Program: {
+        connect: {
+          id: week.programId,
+        },
+      },
+      days: {
+        create: week.days.map(day => ({
+          name: day.name,
+          PlannedExercise: {
+            create: day.PlannedExercise.map(exercise => ({
+              exerciseId: exercise.exerciseId,
+              sets: exercise.sets,
+              reps: exercise.reps,
+              description: exercise.description,
+              order: exercise.order,
+            })),
+          },
+        })),
+      },
+    },
+  });
+}
+
 export async function getWeeks(programId: string) {
   const userId = (await getSSRUserId()) ?? '';
   return prisma.week.findMany({

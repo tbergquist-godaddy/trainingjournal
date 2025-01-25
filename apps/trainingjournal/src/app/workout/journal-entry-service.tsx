d4e8a47e665aff056b59bcf527e1648153f97820
Trond Bergquist <tbergquist@godaddy.com>
2025-01-25 11:53:04 -0500

    show latest/today register

--- apps/trainingjournal/src/app/workout/journal-entry-service.tsx
+++ apps/trainingjournal/src/app/workout/journal-entry-service.tsx
@@ -18,3 +18,33 @@ export async function createJournalEntry(journalEntry: Omit<JournalEntry, 'id'>)
     data: journalEntry,
   });
 }
+
+export type GetLatestJournalEntries = ReturnType<typeof getLatestJournalEntries>;
+
+export async function getLatestJournalEntries(exerciseId: string) {
+  const userId = (await getSSRUserId()) ?? undefined;
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
+
+  const workout = await prisma.workout.findFirst({
+    where: {
+      userId,
+      date: {
+        lt: today,
+      },
+      JournalEntry: {
+        some: {
+          exerciseId,
+        },
+      },
+    },
+    select: {
+      JournalEntry: true,
+    },
+    orderBy: {
+      date: 'desc',
+    },
+  });
+
+  return workout?.JournalEntry.filter(entry => entry.exerciseId === exerciseId);
+}

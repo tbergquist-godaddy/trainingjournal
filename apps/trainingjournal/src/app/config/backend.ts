45e378bf3d36fc8f39bb4bc310ff6736c52b8a10
Trond Bergquist <tbergquist@godaddy.com>
2025-06-14 17:24:25 -0400

    upgrade supertokens

--- apps/trainingjournal/src/app/config/backend.ts
+++ apps/trainingjournal/src/app/config/backend.ts
@@ -3,7 +3,6 @@ import PasswordlessNode from 'supertokens-node/recipe/passwordless';
 import SessionNode from 'supertokens-node/recipe/session';
 import { appInfo } from './appInfo';
 import { TypeInput } from 'supertokens-node/types';
-
 const { SUPERTOKENS_CONNECTION_URL, SUPERTOKENS_API_KEY } = process.env;
 
 if (SUPERTOKENS_CONNECTION_URL === undefined || SUPERTOKENS_API_KEY === undefined) {
@@ -14,7 +13,6 @@ export const backendConfig = (): TypeInput => {
   return {
     framework: 'custom',
     supertokens: {
-      // These are the connection details of the app you created on supertokens.com
       connectionURI: SUPERTOKENS_CONNECTION_URL,
       apiKey: SUPERTOKENS_API_KEY,
     },

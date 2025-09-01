817fd8b1f582c696d24c727437df108a3901180e
Trond Bergquist <tbergquist@godaddy.com>
2025-09-01 07:39:32 -0400

    move delete exercise button down to action buttons

--- apps/trainingjournal/src/app/exercises/edit/[id]/page.tsx
+++ apps/trainingjournal/src/app/exercises/edit/[id]/page.tsx
@@ -8,22 +8,27 @@ import deleteExercise from './actions/delete-exercise-action';
 type Props = {
   params: Promise<{ id: string }>;
 };
+
 export default async function EditExercisePage({ params }: Props) {
   const { id } = await params;
   return (
     <div>
       <Box display="flex" justifyContent="space-between">
         <Typography as="h1">Edit exercise</Typography>
-        <form action={deleteExercise}>
-          <input type="hidden" name="id" value={id} />
-          <Button variant="danger" type="submit">
-            Delete
-          </Button>
-        </form>
       </Box>
       <Section>
         <Suspense fallback="loading">
-          <EditExercise id={id} />
+          <EditExercise
+            extraActions={
+              <form action={deleteExercise}>
+                <input type="hidden" name="id" value={id} />
+                <Button variant="danger" type="submit">
+                  Delete
+                </Button>
+              </form>
+            }
+            id={id}
+          />
         </Suspense>
       </Section>
     </div>

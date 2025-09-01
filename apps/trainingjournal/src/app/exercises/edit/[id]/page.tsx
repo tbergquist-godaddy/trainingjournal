35499f2634f33dc2500e654faf856d9ff1006db7
Trond Bergquist <tbergquist@godaddy.com>
2025-09-01 07:50:12 -0400

    show journal entries for exercise

--- apps/trainingjournal/src/app/exercises/edit/[id]/page.tsx
+++ apps/trainingjournal/src/app/exercises/edit/[id]/page.tsx
@@ -4,13 +4,17 @@ import Typography from '../../../components/typography/typography';
 import EditExercise from './components/edit-exercise';
 import { Button, Box } from '@tbergq/components';
 import deleteExercise from './actions/delete-exercise-action';
+import { getJournalEntriesForExercise } from '../../../services/exercises/exercise-service';
+import JournalEntriesList from './components/journal-entries-list';
 
 type Props = {
   params: Promise<{ id: string }>;
+  searchParams: Promise<{ page?: string }>;
 };
 
-export default async function EditExercisePage({ params }: Props) {
+export default async function EditExercisePage({ params, searchParams }: Props) {
   const { id } = await params;
+  const { page } = await searchParams;
   return (
     <div>
       <Box display="flex" justifyContent="space-between">
@@ -28,6 +32,18 @@ export default async function EditExercisePage({ params }: Props) {
               </form>
             }
             id={id}
+          />
+        </Suspense>
+      </Section>
+      <Section>
+        <Typography size={2} as="h2">
+          Journal Entries
+        </Typography>
+        <Suspense fallback={<div>Loading journal entries...</div>}>
+          <JournalEntriesList
+            journalEntriesPromise={getJournalEntriesForExercise(id, {
+              page: parseInt(page ?? '1', 10),
+            })}
           />
         </Suspense>
       </Section>

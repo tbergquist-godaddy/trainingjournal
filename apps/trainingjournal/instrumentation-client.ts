54463d92d742e7fd828e83beef806c1cea15e695
Trond Bergquist <tbergquist@godaddy.com>
2025-11-26 09:05:51 -0500

    trainingjournal: update dsn for new project

--- apps/trainingjournal/instrumentation-client.ts
+++ apps/trainingjournal/instrumentation-client.ts
@@ -1,12 +1,9 @@
 import * as Sentry from '@sentry/nextjs';
 Sentry.init({
-  dsn: 'https://59f8f41241656edc572e84963169b2d3@o4510431512690688.ingest.us.sentry.io/4510431515246592',
+  dsn: 'https://4ec56e4974c869dfa3cd9cab724699b9@o4510431512690688.ingest.us.sentry.io/4510431658639360',
   // Adds request headers and IP for users, for more info visit:
   // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
   sendDefaultPii: true,
-  // Note: if you want to override the automatic release value, do not set a
-  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
-  // that it will also get attached to your source maps
 });
 // This export will instrument router navigations, and is only relevant if you enable tracing.
 // `captureRouterTransitionStart` is available from SDK version 9.12.0 onwards
--- apps/trainingjournal/sentry.server.config.ts
+++ apps/trainingjournal/sentry.server.config.ts
@@ -1,8 +1,7 @@
 import * as Sentry from '@sentry/nextjs';
 Sentry.init({
-  dsn: 'https://59f8f41241656edc572e84963169b2d3@o4510431512690688.ingest.us.sentry.io/4510431515246592',
+  dsn: 'https://4ec56e4974c869dfa3cd9cab724699b9@o4510431512690688.ingest.us.sentry.io/4510431658639360',
   // Adds request headers and IP for users, for more info visit:
   // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
   sendDefaultPii: true,
-  // that it will also get attached to your source maps
 });
